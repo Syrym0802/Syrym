c456f7147428627b8673febcacd7c51add3d7b42
Syrym0802 <57889262+Syrym0802@users.noreply.github.com>
2019-11-19 12:41:02 +0600

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-  cout<<"Monday"<<endl<<"November"<<endl<<"2019"<<endl<<"Yernur";
+  cout<<"Monday"<<endl<<"November"<<endl<<"2019"<<endl<<"Syrym";
   return 0;
  }
